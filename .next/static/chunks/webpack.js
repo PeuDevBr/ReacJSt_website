555bf612ed2038827a6d81317cbf6359c1014044
Flávio Renato <peudevbr@gmail.com>
2020-12-03 19:05:01 -0300

    Adicionando CSS ao projeto. Adicionando imagem

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0b5029dd9e5c7ecb4475";
+/******/ 	var hotCurrentHash = "e737ca851abc0708a438";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
