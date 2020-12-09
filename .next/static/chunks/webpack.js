bf35bbff810b0adcbcb6db45c7e0c32acaadec69
Flávio Renato <peudevbr@gmail.com>
2020-12-09 19:21:42 -0300

    Verificando erros na Vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a5c3172a3bb4cbb722ae";
+/******/ 	var hotCurrentHash = "874162f39f7ace884a36";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
