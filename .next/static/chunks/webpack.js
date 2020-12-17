d756d0ee474fc63b4430e82d30aa3b5a804f55e5
Flávio Renato <peudevbr@gmail.com>
2020-12-16 23:09:30 -0300

    Modificante a base de dados

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bfb223e3acf156a4deb2";
+/******/ 	var hotCurrentHash = "466236a525a5f9f77226";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
